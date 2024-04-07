20d6133421cfa20f88420f60c0738e117093df73
תהילה הרר < @bybs.local>
2024-04-07 16:39:55 +0300

    Tehila Details2

--- sharanski-project/src/components/our-details.jsx
+++ sharanski-project/src/components/our-details.jsx
@@ -1,5 +1,6 @@
 import { ExempleDetails } from "./exemple-details";
 import { SariDetails } from "./sari-component";
+import { TehilaHDetails } from "./Tehilah_H_Component";
 
 export const OurDetails = () => {
   return (
@@ -17,6 +18,7 @@ export const OurDetails = () => {
         {/* כל אחת טוענת את הקומפוננטה שלה */}
         <ExempleDetails></ExempleDetails>
         <SariDetails></SariDetails>
+        <TehilaHDetails></TehilaHDetails>
       </div>
     </>
   );

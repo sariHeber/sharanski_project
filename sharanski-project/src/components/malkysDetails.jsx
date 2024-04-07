e2200a71b4a23a2ab5113f2dfdfe156b41e731d9
Tami Vahab <tm8000900@gamil.com>
2024-04-07 16:51:48 +0300

    more of Malkys

---
+++ sharanski-project/src/components/malkysDetails.jsx
@@ -0,0 +1,18 @@
+export const MalkysDetails = () => {
+    return (
+      <div
+        style={{
+          borderColor: "green",
+          borderWidth: "5px",
+          borderStyle: "dotted",
+        }}
+      >
+        <h1>name: Malky</h1>
+        <h1>adress: Sorek</h1>
+        <h1>phone: my phone</h1>
+        <h1>mail: my mail</h1>
+        <h1>date of birth: my date of birth</h1>
+      </div>
+    );
+  };
+  

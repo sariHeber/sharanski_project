f3ff3be7d38a1bf0b0d60ebfc937b722780c77e6
naomi racheli <Naomi0534187519@gmail.com>
2024-04-07 16:33:00 +0300

    My ....

--- sharanski-project/src/components/our-details.jsx
+++ sharanski-project/src/components/our-details.jsx
@@ -1,4 +1,5 @@
 import { ExempleDetails } from "./exemple-details";
+import { SaraRuthDetails } from "./Sara-Ruth-conponent";
 import { SariDetails } from "./sari-component";
 
 export const OurDetails = () => {
@@ -17,6 +18,7 @@ export const OurDetails = () => {
         {/* כל אחת טוענת את הקומפוננטה שלה */}
         <ExempleDetails></ExempleDetails>
         <SariDetails></SariDetails>
+        <SaraRuthDetails></SaraRuthDetails>
       </div>
     </>
   );
